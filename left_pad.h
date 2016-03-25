09939c67836e93d6e5c152827b635373d59cbe43
Nelson Castillo <nelsoneci@gmail.com>
2016-03-25 00:06:07 -0500

    Fix copyright. Important.

--- left_pad.h
+++ left_pad.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) <year>, Nelson Castillo
+ * Copyright (c) 2016, Nelson Castillo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
